8d58782078a96df7acdedd5ea6e591697bd3c906
DouradoG <guilherme.s.dourado@gmail.com>
2024-06-23 09:46:58 -0300

    Inserindo dinamica de argumentos - DouradoG

---
+++ Calculadora - Documentacao/CalculadoraDocumentacao/src/ArgumentosArg.java
@@ -0,0 +1,10 @@
+public class ArgumentosArg {
+    public static void main(String[] args) {
+        String nome = args [0];
+        String sobreNome = args[1];
+        
+        System.out.println("Olá meu nome é "+nome);
+        System.out.println("e meu sobre nome é "+sobreNome);
+    }
+    
+}

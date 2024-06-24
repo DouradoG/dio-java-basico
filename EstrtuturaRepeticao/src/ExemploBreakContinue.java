af720d720689d9225aa27236f0ec66649c858fc5
DouradoG <guilherme.s.dourado@gmail.com>
2024-06-24 17:48:29 -0300

    Exemplos de break e continue - DouradoG

--- EstrtuturaRepeticao/src/ExemploBreakContinue.java
+++ EstrtuturaRepeticao/src/ExemploBreakContinue.java
@@ -1,9 +1,31 @@
 public class ExemploBreakContinue {
     public static void main(String[] args) {
-        //int contagem = 0;
+        int contar [] = {0,1,2,3,4,5,6,7,8,9,10};
+        //int numero = 1;
 
-        for( int contagem, contagem >50, contagem ++){
+        for( int resultado : contar ) {
 
+            System.out.println(resultado);
+            if (resultado==6)
+            break;
+        }
+
+        for(int numero = 1; numero <=40 ; numero ++) {
+            if (numero >= 5 && numero <=30)
+            continue;
+
+            System.out.println(numero);
+            
+            
+            
+        }
+
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                continue;
+            
+            System.out.println(numero);
+            
         }
     }
     
